4e374fa7a832b7b60f4bfd0db50c4052ff868c8b
MasterUNG <phrombutr@gmail.com>
2017-08-10 11:54:12 +0700

    Choose Picture Success

--- app/src/main/java/iberla/hirunrattanakorn/surakit/android101/fragment/SignUpFragment.java
+++ app/src/main/java/iberla/hirunrattanakorn/surakit/android101/fragment/SignUpFragment.java
@@ -1,5 +1,9 @@
 package iberla.hirunrattanakorn.surakit.android101.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -20,10 +24,12 @@ import iberla.hirunrattanakorn.surakit.android101.manager.MyAlert;
 public class SignUpFragment extends Fragment {
 
     //Explcit
-    private ImageView backImageView, saveImageView, uploadImageView;
+    private ImageView backImageView, saveImageView,
+            uploadImageView, pictureImageView;
     private EditText nameEditText, userEditText , passwordEditText;
     private String nameString, userString, passwordString;
     private String tag = "10AugV1";
+    private Uri uri;
 
     @Nullable
     @Override
@@ -47,7 +53,52 @@ public class SignUpFragment extends Fragment {
         //Save Controller
         saveController();
 
+        //Picture Controller
+        pictureController();
+
     }   // onActivityCreate
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode,
+                                 Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+            Log.d(tag, "RESULT_OK");
+
+            uri = data.getData();
+
+            //Show Image
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity()
+                        .getContentResolver().openInputStream(uri));
+                pictureImageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                Log.d(tag, "e ShowImage ==> " + e.toString());
+            }
+
+
+        }   // if
+
+    }   // onActivityResult
+
+    private void pictureController() {
+        pictureImageView = getView().findViewById(R.id.imvPicture);
+        pictureImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Choose Picture
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App"), 1);
+
+            }   // onClick
+        });
+    }
 
     private void saveController() {
         saveImageView.setOnClickListener(new View.OnClickListener() {
@@ -98,6 +149,7 @@ public class SignUpFragment extends Fragment {
         userEditText = getView().findViewById(R.id.edtUser);
         passwordEditText = getView().findViewById(R.id.edtPassword);
         uploadImageView = getView().findViewById(R.id.imvUpload);
+        pictureImageView = getView().findViewById(R.id.imvPicture);
 
     }
 
